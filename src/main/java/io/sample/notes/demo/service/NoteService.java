ce90eacbf4d7df22aff7df208d310c7ffd9f3e0f
shiwantha viraj <shiwanvir@gmail.com>
2024-01-11 12:32:04 -0500

    modified exception handling

--- src/main/java/io/sample/notes/demo/service/NoteService.java
+++ src/main/java/io/sample/notes/demo/service/NoteService.java
@@ -19,81 +19,120 @@ public class NoteService {
 
 	// save single note in to database
 	public CustomResponse<Note> saveNote(Note note) {
-		// save note object using JPA
-		Note savedNote = noteRepository.save(note);
-		// check saved status
-		if (savedNote != null) {
-			// if saving successful return the Response with saved object and HTTP status
-			return new CustomResponse<>(savedNote, "Note saved sucessfully", HttpStatus.CREATED.value());
-		} else {
-			// if saving failed return the Response with saved object and HTTP status
-			return new CustomResponse<>(null, "Faild to create note", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		try {
+			// save note object using JPA
+			Note savedNote = noteRepository.save(note);
+			// check saved status
+			if (savedNote != null) {
+				// if saving successful return the Response with saved object and HTTP status
+				return new CustomResponse<>(savedNote, "Note saved sucessfully", HttpStatus.CREATED.value());
+			} else {
+				// if saving failed return the Response with saved object and HTTP status
+				return new CustomResponse<>(null, "Faild to create note", HttpStatus.INTERNAL_SERVER_ERROR.value());
+			}
+		} catch (Exception e) {
+			// Handle any unexpected exceptions that might occur during the save process
+			return new CustomResponse<>(null, "An error occurred while saveing the note " + e.getMessage(),
+					HttpStatus.INTERNAL_SERVER_ERROR.value());
 		}
 	}
 
 	// get all note from database
 	public CustomResponse<List<Note>> getAllNotes() {
-		// get list of existing notes using JPA
-		List<Note> allNotes = noteRepository.findAll();
-		// check if note list is empty
-		if (!allNotes.isEmpty()) {
-			// if note list is not empty return list of notes with HTTP status
-			return new CustomResponse<>(allNotes, "Notes retrived sucessfully", HttpStatus.OK.value());
-		} else {
-			// if list is empty return null with HTTP status
-			return new CustomResponse<>(null, "No notes found", HttpStatus.NOT_FOUND.value());
+		try {
+			// get list of existing notes using JPA
+			List<Note> allNotes = noteRepository.findAll();
+			// check if note list is empty
+			if (!allNotes.isEmpty()) {
+				// if note list is not empty return list of notes with HTTP status
+				return new CustomResponse<>(allNotes, "Notes retrived sucessfully", HttpStatus.OK.value());
+			} else {
+				// if list is empty return null with HTTP status
+				return new CustomResponse<>(null, "No notes found", HttpStatus.NOT_FOUND.value());
+			}
+		} catch (Exception e) {
+			// Handle any unexpected exceptions that might occur during the retrieving
+			// process
+			return new CustomResponse<>(null, "An error occurred while retiving the notes " + e.getMessage(),
+					HttpStatus.INTERNAL_SERVER_ERROR.value());
 		}
-
 	}
 
 	// get one note from database
 
 	public CustomResponse<Note> getNote(int id) {
-		// check if note is available in the database
-		Note note = noteRepository.findById(id).orElse(null);
-		if (note != null) {
-			// if available return note object with HTTP status
-			return new CustomResponse<>(note, "Note retrived sucessfully", HttpStatus.OK.value());
-		} else {
-			// if not available return null with HTTP status
-			return new CustomResponse<>(null, "Note not found", HttpStatus.NOT_FOUND.value());
+		try {
+			// check if note is available in the database
+			Note note = noteRepository.findById(id).orElse(null);
+			if (note != null) {
+				// if available return note object with HTTP status
+				return new CustomResponse<>(note, "Note retrived sucessfully", HttpStatus.OK.value());
+			} else {
+				// if not available return null with HTTP status
+				return new CustomResponse<>(null, "Note not found", HttpStatus.NOT_FOUND.value());
+			}
+		} catch (Exception e) {
+			// Handle any unexpected exceptions that might occur during the retrieving
+			// process
+			return new CustomResponse<>(null, "An error occurred while retiving the note " + e.getMessage(),
+					HttpStatus.INTERNAL_SERVER_ERROR.value());
+
 		}
 	}
 
 	// Update note from database
 
 	public CustomResponse<Note> updateNote(Note note) {
-		// check if note is available in the database
-		if (noteRepository.existsById(note.getId())) {
-			// if note is available assign it to note object
-			Note exsistingNote = noteRepository.findById(note.getId()).orElse(null);
-			// update note object with new values
-			exsistingNote.setTitle(note.getTitle());
-			exsistingNote.setDescription(note.getDescription());
-			// save updated object in to database
-			Note updatedNote = noteRepository.save(exsistingNote);
-			// return response with updated object and HTTP status
-			return new CustomResponse<>(updatedNote, "Note updated sucessfully", HttpStatus.OK.value());
+		try {
+			// check if note is available in the database
+			if (noteRepository.existsById(note.getId())) {
+				// if note is available assign it to note object
+				Note exsistingNote = noteRepository.findById(note.getId()).orElse(null);
+				// update note object with new values
+				exsistingNote.setTitle(note.getTitle());
+				exsistingNote.setDescription(note.getDescription());
+				// save updated object in to database
+				Note updatedNote = noteRepository.save(exsistingNote);
+				// return response with updated object and HTTP status
+				return new CustomResponse<>(updatedNote, "Note updated sucessfully", HttpStatus.OK.value());
 
-		} else {
-			// if note is not found return empty object with HTTP status
-			return new CustomResponse<>(null, "Note not found for updating", HttpStatus.NOT_FOUND.value());
+			} else {
+				// if note is not found return empty object with HTTP status
+				return new CustomResponse<>(null, "Note not found for updating", HttpStatus.NOT_FOUND.value());
+			}
+
 		}
 
+		catch (Exception e) {
+			// Handle any unexpected exceptions that might occur during the update process
+			return new CustomResponse<>(null, "An error occurred while updating the note" + e.getMessage(),
+					HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+		}
 	}
 
 	// Delete note
 
 	public CustomResponse<String> deleteNote(int id) {
-		// check note is available in the database by id
-		if (noteRepository.existsById(id)) {
-			// if note is available delete note
-			noteRepository.deleteById(id);
-			// return success message with HTTP status
-			return new CustomResponse<>(null, ("Note with ID " + id + " deleted successfully."), HttpStatus.OK.value());
-		} else {
-			// if deletion valid return error response with HTTP status
-			return new CustomResponse<>(null, "Note not found", HttpStatus.NOT_FOUND.value());
+		try {
+			// check note is available in the database by id
+			if (noteRepository.existsById(id)) {
+				// if note is available delete note
+				noteRepository.deleteById(id);
+				// return success message with HTTP status
+				return new CustomResponse<>(null, ("Note with ID " + id + " deleted successfully."),
+						HttpStatus.OK.value());
+			} else {
+				// if deletion valid return error response with HTTP status
+				return new CustomResponse<>(null, "Note not found", HttpStatus.NOT_FOUND.value());
+			}
+		}
+
+		catch (Exception e) {
+			// Handle any unexpected exceptions that might occur during the deletion process
+			return new CustomResponse<>(null, "An error occurred while updating the note " + e.getMessage(),
+					HttpStatus.INTERNAL_SERVER_ERROR.value());
+
 		}
 	}
 
